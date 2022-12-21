caf4541749463cbc6d949a0f187707c7e8d50186
pendoagnes222 <pendoagnes22@gmail.com>
2022-12-21 17:07:59 +0300

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-
   const photo = uploadPhoto();
   const user = createUser();
-
-  return Promise.all([photo, user])
-    .then((data) => {
-      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+  return Promise.all([photo, user]).then((values) => console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`)).catch(() => console.log('Signup system offline'));
 }
