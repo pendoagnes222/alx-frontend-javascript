546c17ccb4a892f1c57b2247469e9e3431b0c658
pendoagnes222 <pendoagnes22@gmail.com>
2022-12-21 17:06:11 +0300

    3. Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
 
   const photo = uploadPhoto();
   const user = createUser();
- 
+
   return Promise.all([photo, user])
     .then((data) => {
       console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
