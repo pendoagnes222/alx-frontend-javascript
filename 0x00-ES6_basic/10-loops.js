7a4d18e1e4b1721805d3d05d5353fd77b1836c63
pendoagnes222 <pendoagnes22@gmail.com>
2022-12-21 06:28:45 +0300

    10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const value = [];
+  for (const idx of value) {
+    value.push(appendString + idx);
+  }
+
+  return value;
+}
