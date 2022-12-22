f73366dd5fbcbba5cd98e403c1e397dba4541d0c
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2022-12-22 23:01:43 +0300

    5. A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,14 +1,13 @@
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
-    if (
-      this.constructor !== Building &&
-      this.evacuationWarningMessage === undefined
-    ) {
-      throw Error(
-        "Class extending Building must override evacuationWarningMessage"
-      );
-    }
+    /* eslint no-underscore-dangle: ["error", { "allow": ["_sqft"] }] */
+    this._sqft = sqft;
+    const proto = Object.getPrototypeOf(this);
+    const superProto = Building.prototype;
+    /* eslint no-prototype-builtins: "error" */
+    const missing = Object.getOwnPropertyNames(superProto).find((name) => typeof superProto[name] === 'function' && !Object.prototype.hasOwnProperty.call(proto, name));
+
+    if (missing) throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 
   get sqft() {
@@ -17,5 +16,9 @@ export default class Building {
 
   set sqft(value) {
     this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    return `evacuationWarningMessage ${this._sqft}`;
   }
 }
