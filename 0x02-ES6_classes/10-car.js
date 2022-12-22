6186ec2d05c4dcec6b353dc91e0a91868da36760
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2022-12-22 23:11:33 +0300

    10. Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,6 +1,5 @@
 export default class Car {
   constructor(brand, motor, color) {
-    /* eslint no-underscore-dangle: ["error", { "allow": ["_brand", "_motor", "_color"] }] */
     this._brand = brand;
     this._motor = motor;
     this._color = color;
