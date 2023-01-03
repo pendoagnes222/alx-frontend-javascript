12c446e37c07a3510f28294d1074af662c45d05e
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-01-03 23:31:50 +0300

    4. Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,6 +1,5 @@
 export default function updateStudentGradeByCity(studentList, city, newGrade) {
   const newArray = studentList.filter((val) => val.location === city).map((val) => {
-    /* eslint no-param-reassign: ["error", { "props": false }] */
     val.grade = 'N/A';
     for (let i = 0; i < newGrade.length; i += 1) {
       if (val.id === newGrade[i].studentId) {
