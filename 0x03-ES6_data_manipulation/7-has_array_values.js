bd3649ba0285bb0dd702ada134d1c84714786712
PENDO AGNES <99383447+pendoagnes222@users.noreply.github.com>
2023-01-03 23:38:57 +0300

    7. More set data structure

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,3 +1,3 @@
-export default function hasValuesFromArray(inputSet, inputArray) {
-  return inputArray.every((value) => inputSet.has(value));
+export default function hasValuesFromArray(Set, inputArray) {
+  return inputArray.every((value) => Set.has(value));
 }
